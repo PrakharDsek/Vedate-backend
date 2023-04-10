3376efbde71926b6a49c497037f4eb070500b512
PrakharDsek <105925167+PrakharDsek@users.noreply.github.com>
2023-04-10 09:59:59 +0530

    removed Auth

--- routes/task.js
+++ routes/task.js
@@ -1,14 +1,17 @@
-import express from "express"
-import { createTask, deleteTask, getTasks, updateTaskStatus } from "../controllers/task.js"
-import { isAuth } from "../middlewares/auth.js"
+import express from "express";
+import {
+  createTask,
+  deleteTask,
+  getTasks,
+  updateTaskStatus,
+} from "../controllers/task.js";
+import { isAuth } from "../middlewares/auth.js";
 
-const router=express.Router()
+const router = express.Router();
 
-router.post("/new",isAuth, createTask)
-router.post("/allTask/:userId",getTasks)
-router.put("/update/:id",isAuth, updateTaskStatus)
-router.delete("/delete/:id",isAuth, deleteTask)
+router.post("/new", isAuth, createTask);
+router.post("/allTask/:userId", getTasks);
+router.put("/update/:id", updateTaskStatus);
+router.delete("/delete/:id", deleteTask);
 
-
-
-export default router
+export default router;
